0a74d306ecbebfb4deabefa85c2499f835753442
darkapple9975 <darkapple9975@gmail.com>
2025-07-22 18:06:37 -0400

    Initialize project

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+Keep track of your warframe companions using Companion Manager. Intended for breeders.
+Copyright (C) 2025  darkapple
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#include <QApplication>
+
+#include "mainWindow.h"
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+    ui::MainWindow hello;
+    hello.show();
+    return app.exec();
+}
